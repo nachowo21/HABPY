8ae83947f766eeac07088106a89647d5d7eceb32
nachowo21 <ignacio.solis.m@usach.cl>
2023-07-17 18:48:53 -0400

    ADD-agregue mas comentados para el codigo

--- funcion_2.py
+++ funcion_2.py
@@ -10,11 +10,11 @@ def zona_habitable(T_estrella,radio_estrella):
     d_externa = (radio_estrella/2)*((T_estrella/273)**2)
     return (d_interna,d_externa) 
 
-t_estrella= float(input('ingrese la temperatura de la estrella en kelvin: '))
-radio_estrella = float(input('ingrese el radio de la estrella en kilometros: '))
-zona_h = zona_habitable(t_estrella,radio_estrella)
+t_estrella= float(input('ingrese la temperatura de la estrella en kelvin: ')) # le pedimos al usuario que ingrese la temperatura en kelvin (este será un parametro flotante)
+radio_estrella = float(input('ingrese el radio de la estrella en kilometros: '))# al igual que t_estrella este también seerá ingresado por el usuario y será flotante
+zona_h = zona_habitable(t_estrella,radio_estrella) # aplicamos la función previamente definida
 
-print('la zona de habitabilidad de su estrella se encuentra entre: ', zona_h, 'kilometros')
+print('la zona de habitabilidad de su estrella se encuentra entre: ', zona_h, 'kilometros')# aqui nos imprime en la terminal los valores de los radios habitables
 
 centro_x = 0 # definimos la posicion (en coordenadas cartesianas) de la estrella
 centro_y = 0
@@ -34,7 +34,7 @@ plt.plot(x_exterior,y_exterior, label = 'exterior') # con ayuda de matplotlib.py
 plt.plot(x_interior,y_interior, label = 'interior')
 
 plt.fill([*x_exterior, *x_interior[::-1]], [*y_exterior, *y_interior[::-1]], color='green', alpha=0.5,label ='zona habitable')  # Rellenar el segmento entre las curvas
-plt.scatter(centro_x,centro_y,color = 'red' ,label = 'star')
+plt.scatter(centro_x,centro_y,color = 'red' ,label = 'star') # creamos un punto en el eje 0,0 el cual será nuestra estrella 
 plt.axis('equal')  
 plt.title('zona habitable')       
 plt.ylabel('y [AU]')
