b7a70e2bac562c1ef076459a99952817c3f1bf2b
nachowo21 <ignacio.solis.m@usach.cl>
2023-08-03 13:27:08 -0400

    ADD-añadí la carpeta liciencia, pyproject.toml y setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,16 @@
-import setuptools
+from setuptools import setup #no es necesario añadirlo como dependencia
 
-#Si tienes un readme
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh: #saltarselo si no tienes el README en la carpeta del projecto
     long_description = fh.read()
 
-
-setuptools.setup(
-     name='HABZONEpy',  #nombre del paquete
-     version='0.1', #versión
-     scripts=['zona_habitable.py'] , #nombre del ejecutable
-     author="ignacio solis", #autor
-     author_email="ignacio.solis.m@usach.cl", #email
-     description="un paquete para encontrar la zona habitable de una estrella", #Breve descripción
-     long_description=long_description,
-   long_description_content_type="text/markdown", #Incluir el README.md si lo has creado
-     url="https://github.com/nachowo21/HABZONEpy.git", #url donde se encuentra tu paquete en Github
-     packages=setuptools.find_packages(), #buscamos todas las dependecias necesarias para que tu paquete funcione (por ejemplo numpy, scipy, etc.)
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- ) #aquí añadimos información sobre el lenguaje usado, el tipo de licencia, etc.
-
+setup(
+      name = 'HABZONEpy', # aqui el nombre de la carpeta donde esta tu modulo/archivo/programa
+      version = '1.0.2', # la version correspondiente del programa, VERSION, no commit de github
+      license = 'MIT',
+      description = 'codigo para visualizar y encontrar la zona habitable', # breve descripcion
+      long_description = long_description,
+      long_description_content_type = 'text/markdown',
+      author = 'Ignacio Solís, Joaquin Lopez, Ignacio Cordova',
+      install_requires = ['numpy', 'matplotlib'], #aqui van tus dependecias de tu programa, como numpy, pandas, cosas asi, esto hara que se instalen solos cuando instalen tu modulo si es que no tienen los necesarios
+      url = 'https://github.com/nachowo21/HABZONEpy.git', # url de tu github, es opcional pero queda bien
+      )
